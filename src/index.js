851d455e8d2bc579d49fd2ddc181cd3d960c5e3a
Arano Orkatz <oarano@lekooa.com>
2017-10-20 20:50:23 +0200

    create connexion components

--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import React from 'react';
 import { render } from 'react-dom';
 //CSS
 import './index.css';
+//Components
+import Connexion from './components/Connexion';
 
-
-class Connexion extends React.Component {
-    render() {
-        return (
-            <div className="connexionBox">
-                <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required />
-                    <button type="submit">Go</button>
-                </form>
-            </div>
-        )
-    }
-}
 
 render(
     <Connexion />,
     document.getElementById('root')
 );
+
